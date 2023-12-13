846eaafef3712adb29938dd5b2c19b3573dcc77e
andrea <okeena.elders@gmail.com>
2023-12-13 17:04:50 +0530

    view details of employees

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import ViewDetails from "./ViewDetails";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
 
   const navigateToViewDetails = () => {
-    navigate("/ViewDetails"); // Adjust the path based on your route configuration
+    navigate("/ViewDetails"); 
   };
 
   const navigateToForm = () => {
-    navigate("/Form"); // Adjust the path based on your route configuration
+    navigate("/Form"); 
   };
 
   return (
     <div>
       <h1>Employee System</h1>
-      <button className="1" onClick={navigateToViewDetails}>
+      <button onClick={navigateToViewDetails}>
         View Employee Details
       </button>
       <button onClick={navigateToForm}>Add Employee Details</button>
--- src/pages/ViewDetails.tsx
+++ src/pages/ViewDetails.tsx
@@ -18,22 +18,21 @@ const ViewDetails: React.FC = () => {
         "Content-Type": "application/json"
       }
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((json: Employee[]) => {
-        setEmployees(json);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error(`HTTP error! Status: ${response.status}`);
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((json: Employee[]) => {
+    //     setEmployees(json);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error fetching data:", error);
+    //   });
   };
 
   useEffect(() => {
-    // Call apiGet when the component mounts
     apiGet();
   }, []);
 
